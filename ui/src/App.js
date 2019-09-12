066422cc76b94123b56e2c790460e468530eb990
Brayan <brgmarchena@gmail.com>
2019-09-12 10:33:07 -0600

    Update App.js
    send default machine value from App variables

--- ui/src/App.js
+++ ui/src/App.js
@@ -15,9 +15,9 @@ import * as qs from 'query-string';
 function App(propsApp) {
   // set default machine and type
   const { t } = useTranslation();
-    const machine = propsApp.machine || localStorage.getItem('machine_name') || 'CR2080435W1';
-    const machineData = axios.get(`${API}/asset_display_system?st=${machine}`, { headers: { Authorization: 'Bearer ' + localStorage.getItem('accessToken') }});
-    machineData.then((data) => localStorage.setItem('machineKey', JSON.stringify(data.data[0].AssetDisplaySystem)));
+  const machine = propsApp.machine || localStorage.getItem('machine_name') || 'CR2080435W1';
+  const machineData = axios.get(`${API}/asset_display_system?st=${machine}`, { headers: { Authorization: 'Bearer ' + localStorage.getItem('accessToken') } });
+  machineData.then((data) => localStorage.setItem('machineKey', JSON.stringify(data.data[0].AssetDisplaySystem)));
   return (
     <Router>
       <Helmet>
@@ -26,17 +26,18 @@ function App(propsApp) {
         <meta name="theme-color" content="#ccc" />
       </Helmet>
       <Suspense fallback={<Spinner />}>
-        <Route 
-          path="/dashboard" 
-            render={function(props) {
-              return (
-              <DashboardOne 
-              user={propsApp.user} t={t} 
-              defaultAsset={propsApp.defaultAsset} 
-              history={props.history} 
-              search={qs.parse(props.history.location.search)} />
-              )}
-            } />
+        <Route
+          path="/dashboard"
+          render={function (props) {
+            return (
+              <DashboardOne
+                user={propsApp.user} t={t}
+                defaultAsset={machine}
+                history={props.history}
+                search={qs.parse(props.history.location.search)} />
+            )
+          }
+          } />
         <Route exact path="/login" render={() => <Login t={t} />} />
         <Route exact path="/" render={() => <SignIn t={t} />} />
       </Suspense>
