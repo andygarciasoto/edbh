bfa15f2d7777362a61365b7ded360a660a93c60b
Brayan <brgmarchena@gmail.com>
2020-02-10 11:14:03 -0600

    Fix import name module
    Fix import name module

--- ui/src/Dashboard/Import.js
+++ ui/src/Dashboard/Import.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Row, Col, Tabs, Tab } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import '../Layout/Header.scss';
 import { DATATOOL } from '../Utils/Constants';
 import { saveAs } from 'file-saver';
 import Spinner from '../Spinner';
+import ConfigurationTab from '../Layout/ConfigurationTab';
 import { post } from 'axios';
 const ACCESS_TOKEN_STORAGE_KEY = 'accessToken';
 
@@ -16,6 +17,10 @@ class Import extends React.Component {
     getInitialState(props) {
         return {
             file: {},
+            selectedAction: 'Import',
+            completeListTabs: [],
+            availableListTabs: [],
+            selectedListTabs: [],
             message: '',
             isLoading: false,
             isExporting: false,
@@ -39,19 +44,43 @@ class Import extends React.Component {
             exportingText: props.t('Exporting'),
             successExportMessage: props.t('Configuration Exported Successfully'),
             warningExportMessage: props.t('Fail, Configuration not Exported'),
+            importExportTitleText: props.t('Site Configuration Import/Export Tool'),
+            selectText: props.t('Select an action')
         };
     }
 
     componentDidMount() {
-        this["_isMounted"] = true;
+        this.fetchData();
     }
 
-    componentWillUnmount() {
-        this["_isMounted"] = false;
+    fetchData() {
+        let actualTabs = [];
+
+        actualTabs.push({ id: 'Asset', content: 'Asset' });
+        actualTabs.push({ id: 'DTReason', content: 'DTReason' });
+        actualTabs.push({ id: 'Shift', content: 'Shift' });
+        actualTabs.push({ id: 'Tag', content: 'Tag' });
+        actualTabs.push({ id: 'CommonParameters', content: 'CommonParameters' });
+        actualTabs.push({ id: 'UOM', content: 'UOM' });
+        actualTabs.push({ id: 'Unavailable', content: 'Unavailable' });
+        actualTabs.push({ id: 'TFDUsers', content: 'TFDUsers' });
+
+        this.setState({
+            completeListTabs: actualTabs,
+            availableListTabs: actualTabs
+        })
     }
 
     componentWillReceiveProps(nextProps) {
         this.setState(this.getTranslations(nextProps));
+    }
+
+    handleInputChange = (changeEvent) => {
+        this.setState({
+            selectedAction: changeEvent.target.value,
+            availableListTabs: changeEvent.target.value === 'Export' ? [] : this.state.completeListTabs,
+            selectedListTabs: changeEvent.target.value === 'Export' ? this.state.completeListTabs : [],
+        });
     }
 
     onFileChange = (event) => {
@@ -59,11 +88,25 @@ class Import extends React.Component {
         this.setState({ file });
     }
 
+    updateTabsImported = (availableListTabs, selectedListTabs) => {
+        this.setState({ availableListTabs, selectedListTabs });
+    }
+
+    importAllTabs = () => {
+        this.updateTabsImported([], this.state.completeListTabs);
+    };
+
+    resetTabs = () => {
+        this.updateTabsImported(this.state.completeListTabs, []);
+    }
+
     onSubmit = () => {
         let _this = this;
         const url = `${DATATOOL}/import_asset`;
         let formData = new FormData();
         formData.append('file', _this.state.file);
+        formData.append('configurationItems', JSON.stringify(_this.state.selectedListTabs));
+        formData.append('site_id', JSON.stringify(_this.props.user.site));
         const config = {
             headers: {
                 Authorization: 'Bearer ' + localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY),
@@ -108,56 +151,60 @@ class Import extends React.Component {
             <div className="wrapper-main-import" style={{ height: '100vh' }}>
                 <Row></Row>
                 <div className="import-wrapper">
-                    <Tabs defaultActiveKey='import' id='uncontrolled-tab-example'>
-                        <Tab eventKey='import' title={this.state.importText}>
+                    <Row>
+                        <h3>{this.props.user.site_name + ' ' + this.state.importExportTitleText}</h3>
+                    </Row>
+                    <Row className='show-grid'>
+                        <Col>
+                            <label>{this.state.selectText + ':'}</label>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={1}>
+                            <label>
+                                <input type="radio" value="Import" checked={this.state.selectedAction === 'Import'}
+                                    onChange={(e) => this.handleInputChange(e)} />
+                                {' ' + this.state.importText}
+                            </label>
+                        </Col>
+                        <Col md={1}>
+                            <label>
+                                <input type="radio" value="Export" checked={this.state.selectedAction === 'Export'}
+                                    onChange={(e) => this.handleInputChange(e)} />
+                                {' ' + this.state.exportText}
+                            </label>
+                        </Col>
+                    </Row>
+                    {this.state.selectedAction === 'Import' ?
+                        <Row className='show-grid'>
                             <Col>
-                                <h4>Import Section</h4>
-                                <p>This section is to import a new configuration to the application. Always the data will be import it using the site of the user that made the request. This keeps the security of the information and changes only the necessary information of the actual user login in.
-The user will update all these configuration tables from the database:
-</p>
-                                <ul>
-                                    <li>DT Reason</li>
-                                    <li>Asset</li>
-                                    <li>Shift</li>
-                                    <li>Tag</li>
-                                    <li>Common Parameters</li>
-                                    <li>UOM</li>
-                                    <li>Unavailable</li>
-                                </ul>
-                                <p>* The configuration can be loaded only from an excel file. The file will have a tab with each table to import. This file needs to have the same information on the export file and will update the configuration in the application.</p>
-                                <div id="UploadFile" className="">
-                                    <input type="file" name="Open File" id="" accept=".xlsx,.xlsm" style={{ fontWeight: 'bold' }} onChange={this.onFileChange} />
-                                    <div style={{ marginTop: '25px' }}>
-                                        <button onClick={this.onSubmit} disabled={this.state.isLoading || !this.state.file.name} style={{ marginTop: "25px", marginRight: "-15px", width: "100px", height: "40px", fontSize: "16px" }}>
-                                            {this.state.isLoading ? this.state.importingText : this.state.importText}
-                                        </button>
-                                    </div>
-                                    <div style={{ marginTop: "20px", fontSize: "17px", width: "45%", color: this.state.error ? 'red' : 'green', fontWeight: 'bold' }}><p>{this.state.showActionMessage ? (this.state.error ? this.state.warningImportMessage : this.state.successImportMessage) : null}</p></div>
-                                </div>
+                                <input type="file" name="Open File" id="" accept=".xlsx,.xlsm" style={{ fontWeight: 'bold' }} onChange={this.onFileChange} />
                             </Col>
-                        </Tab>
-                        <Tab eventKey='export' title={this.state.exportText}>
-                            <h4>Export Section</h4>
-                            <p>This section is to export the actual configuration of the application. Always the data will be export it filtering the information by the site of the user that made the request. This keeps the security of the information and displays only the necessary information of the actual user login in.
-    The user only has the possible to download all these configuration tables from the database:</p>
-                            <ul>
-                                <li>DT Reason</li>
-                                <li>Asset</li>
-                                <li>Shift</li>
-                                <li>Tag</li>
-                                <li>Common Parameters</li>
-                                <li>UOM</li>
-                                <li>Unavailable</li>
-                            </ul>
-                            <p>* The configuration will be saved as an excel file. The file will have a tab with each table downloaded. This file can be used in the import module but any changes in the excel will update the configuration in the application.</p>
-                            <button onClick={() => this.exportEvent()} disabled={this.state.isExporting} style={{ marginTop: "25px", marginRight: "-15px", width: "150px", height: "40px", fontSize: "16px" }}>
-                                {this.state.isExporting ? this.state.exportingText : this.state.exportText}
-                            </button>
-                            {this.state.isExporting ? <Spinner /> : ''}
-                            <div style={{ marginTop: "20px", fontSize: "17px", width: "45%", color: this.state.errorExport ? 'red' : 'green', fontWeight: 'bold' }}><p>{this.state.showActionMessageExport ? (this.state.errorExport ? this.state.warningExportMessage : this.state.successExportMessage) : null}</p></div>
-                        </Tab>
-                    </Tabs>
-                </div >
+                        </Row>
+                        : null}
+                    <br />
+                    <ConfigurationTab
+                        availableListTabs={this.state.availableListTabs}
+                        selectedListTabs={this.state.selectedListTabs}
+                        selectedAction={this.state.selectedAction}
+                        onUpdateTabsImported={this.updateTabsImported}
+                        importAllTabs={this.importAllTabs}
+                        resetTabs={this.resetTabs}
+                        height={'350px'}
+                        t={this.props.t} />
+
+                    {this.state.selectedAction === 'Import' ?
+                        <button onClick={this.onSubmit} disabled={this.state.isLoading || !this.state.file.name} style={{ marginTop: "25px", marginRight: "-15px", width: "150px", height: "40px", fontSize: "16px" }}>
+                            {this.state.isLoading ? this.state.importingText : this.state.importText}
+                        </button>
+                        :
+                        <button onClick={this.exportEvent} disabled={this.state.isExporting} style={{ marginTop: "25px", marginRight: "-15px", width: "150px", height: "40px", fontSize: "16px" }}>
+                            {this.state.isExporting ? this.state.exportingText : this.state.exportText}
+                        </button>
+                    }
+                    <div style={{ marginTop: "20px", fontSize: "17px", width: "45%", color: this.state.error ? 'red' : 'green', fontWeight: 'bold' }}><p>{this.state.showActionMessage ? (this.state.error ? this.state.warningImportMessage : this.state.successImportMessage) : null}</p></div>
+                    <div style={{ marginTop: "20px", fontSize: "17px", width: "45%", color: this.state.errorExport ? 'red' : 'green', fontWeight: 'bold' }}><p>{this.state.showActionMessageExport ? (this.state.errorExport ? this.state.warningExportMessage : this.state.successExportMessage) : null}</p></div>
+                </div>
             </div >)
     }
 
