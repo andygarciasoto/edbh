5efeffac7a0d29a59ebf2f906606fe8e9135015e
Andres Garcia <andy12garsa@gmail.com>
2020-08-31 12:23:11 -0600

    added setup as a dtreason

--- api/src/repositories/dtreason-repository.ts
+++ api/src/repositories/dtreason-repository.ts
@@ -11,6 +11,9 @@ export class DTReasonRepository {
     public async getTimelostReasons(asset_id: number, type: string): Promise<any> {
         return await this.sqlServerStore.ExecuteQuery(`exec dbo.spLocal_EY_DxH_Get_DTReason ${asset_id}, '${type}'`);
     }
+    public async getSetupReason(asset_code: string): Promise<any> {
+        return await this.sqlServerStore.ExecuteQuery(`exec dbo.spLocal_EY_DxH_Get_DTReason_Setup ${asset_code}`);
+    }
     public async getTimelostDxhData(dxh_data_id: number, productiondata_id: number): Promise<any> {
         return await this.sqlServerStore.ExecuteQuery(`exec dbo.spLocal_EY_DxH_Get_DTData_By_DxHData_Id ${dxh_data_id}, ${productiondata_id}`);
     }
