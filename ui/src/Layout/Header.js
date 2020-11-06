f1a80e854e7c2dfe0e6042c91befa3a53208e3ff
Brayan <brayan.gonzalez.marchena@cr.ey.com>
2020-11-06 12:57:29 -0600

    Merge remote-tracking branch 'origin/master-deploy' into parkert_test

--- ui/src/Layout/Header.js
+++ ui/src/Layout/Header.js
@@ -219,7 +219,7 @@ class Header extends React.Component {
                             </span>
                             : null}
                         {isComponentValid(this.props.user.role, 'neworder') && this.props.history.location.pathname !== '/summary' ?
-                            ((this.state.tp) && (this.state.tp) !== '' && (this.state.tp === 'Manual')) ?
+                            ((this.state.tp) && (this.state.tp) !== '' && (this.state.tp === 'Manual' || this.state.tp === 'Partially_Manual_Scan_Order')) ?
                                 <Nav.Link onClick={() => this.props.openModal(true)}>{this.props.t('New Order')} <FontAwesome name="file-text" />
                                 </Nav.Link>
                                 : null : null
