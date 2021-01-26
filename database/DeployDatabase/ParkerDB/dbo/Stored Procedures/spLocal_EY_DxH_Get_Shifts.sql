9c3d6a9e491141173ba39413e80b0faf71741864
adrianm <adrian.mendez@cr.ey.com>
2021-01-25 19:11:39 -0600

    Add new ParkerDB Project / Remove Parker PROD and Test

---
+++ database/DeployDatabase/ParkerDB/dbo/Stored Procedures/spLocal_EY_DxH_Get_Shifts.sql
@@ -0,0 +1,42 @@
+﻿
+--exec dbo.spLocal_EY_DxH_Get_Shifts 1
+
+ CREATE   PROCEDURE [dbo].[spLocal_EY_DxH_Get_Shifts] (@Site as int)
+
+ AS  BEGIN 
+ DECLARE
+ @CurrentDateTime			DATETIME,
+ @CurrentProductionDay		DATETIME,
+ @YesterdayProductionDay	DATETIME,
+ @TomorrowProductionDay		DATETIME,
+ @DateOfShift				DATETIME;
+
+SELECT @CurrentDateTime = SYSDATETIME() at time zone 'UTC' at time zone site_timezone
+FROM dbo.CommonParameters where site_id = @Site;
+SET @CurrentProductionDay = FORMAT(@CurrentDateTime, 'yyyy-MM-dd');
+SET @YesterdayProductionDay = FORMAT(DATEADD(DAY, -1, @CurrentDateTime), 'yyyy-MM-dd');
+SET @TomorrowProductionDay = FORMAT(DATEADD(DAY, 1, @CurrentDateTime), 'yyyy-MM-dd');
+
+
+            SELECT shift_id, 
+                   shift_code, 
+                   shift_name, 
+                   shift_sequence, 
+                   DATEPART(hour, start_time) AS hour,
+				   [start_time_offset_days],
+				   DATEPART(hour, end_time) AS end_hour,
+				   [end_time_offset_days],
+                   [duration_in_minutes],
+                   --GET INTERVAL SHIFT FOR TODAY
+				   FORMAT(DATEADD(HOUR, DATEPART(HOUR, start_time), DATEADD(DAY, start_time_offset_days, @CurrentProductionDay)), 'yyyy-MM-dd HH:mm') AS start_date_time_today,
+				   FORMAT(DATEADD(HOUR, DATEPART(HOUR, end_time), DATEADD(DAY, end_time_offset_days, @CurrentProductionDay)), 'yyyy-MM-dd HH:mm') AS end_date_time_today,
+				   --GET INTERVAL SHIFT FOR YESTERDAY
+				   FORMAT(DATEADD(HOUR, DATEPART(HOUR, start_time), DATEADD(DAY, start_time_offset_days, @YesterdayProductionDay)), 'yyyy-MM-dd HH:mm') AS start_date_time_yesterday,
+				   FORMAT(DATEADD(HOUR, DATEPART(HOUR, end_time), DATEADD(DAY, end_time_offset_days, @YesterdayProductionDay)), 'yyyy-MM-dd HH:mm') AS end_date_time_yesterday,
+				   --GET INTERVAL SHIFT FOR TOMORROW
+				   FORMAT(DATEADD(HOUR, DATEPART(HOUR, start_time), DATEADD(DAY, start_time_offset_days, @TomorrowProductionDay)), 'yyyy-MM-dd HH:mm') AS start_date_time_tomorrow,
+				   FORMAT(DATEADD(HOUR, DATEPART(HOUR, end_time), DATEADD(DAY, end_time_offset_days, @TomorrowProductionDay)), 'yyyy-MM-dd HH:mm') AS end_date_time_tomorrow
+				   FROM [dbo].[Shift]
+				   WHERE STATUS = 'Active' AND asset_id = @site ORDER BY shift_sequence;
+
+END
---
+++ database/DeployDatabase/ParkerDB/dbo/Stored Procedures/spLocal_EY_DxH_Put_DTData.sql
@@ -0,0 +1,270 @@
+﻿
+--
+-- Copyright © 2019 Ernst & Young LLP
+-- All Rights 
+-- spLocal_EY_DxH_Put_DTData
+--
+--  Purpose:
+--	Given a dxhdata_id and downtime info, store the DTData 
+--
+--	Note that this does some data validation
+--		Minutes must not be negative
+--		Minutes must not be greater than 60
+-- 
+--	This code DOES NOT manage the total minutes
+--
+--	Returns a status and a message
+--		 0 is pass
+--		-1 is fail
+--		
+--
+--	To Do:
+--
+--  Output Parameters:
+---
+--  Input Parameters:
+---
+---	
+--  Trigger:
+---
+--  Data Read Other Inputs:  
+--- 
+---	
+--  Data Written Results:
+---
+--  Assumptions:
+--- 
+--  Dependencies: 
+---	None
+---
+--  Variables:
+---
+---
+--  Tables Modified:
+--	DTData
+---
+-- Modification Change History:
+--------------------------------------------------------------------------------
+--	20190814		C00V00 - Intial code created		
+--	20190820		C00V01 - Timestamp used as entered_on for inserts		
+--	20190822		C00V02 - Timestamp adjusted to UTC to be used as entered_on 
+--	20191204		C00V03 - Change CommonParameters to CommonParameters
+--	20200225		C00V04 - Change out result for noraml variables, remove JSON response
+--		
+-- Example Call:
+-- Exec spLocal_EY_DxH_Put_DTData 3, 4, 5, '3276', Null, Null, '2019-08-09 15:08:28.220', Null
+--
+CREATE PROCEDURE [dbo].[spLocal_EY_DxH_Put_DTData]
+--Declare
+@DxHData_Id   INT, -- the hour Id
+@DTReason_Id  INT, 
+@DTMinutes    FLOAT, 
+@Clock_Number VARCHAR(100), -- used to look up First and Last, leave Null if you have first and last
+@First_Name   VARCHAR(100), -- Leave Null if you send Clock Number
+@Last_Name    VARCHAR(100), -- Leave Null if you send Clock Number
+@Timestamp    DATETIME, -- generally the current time in site timezone
+@Update       INT				-- generally Null or 0, send dtdata_id to update
+
+AS
+
+    --Select @DxHData_Id = 3,
+    --	@DTReason_Id = 4, 
+    --	@DTMinutes = 5,
+    --	@Clock_Number = '3276',
+    --	@Timestamp = getdate()
+
+    BEGIN
+        -- SET NOCOUNT ON added to prevent extra result sets from
+        -- interfering with SELECT statements.
+        SET NOCOUNT ON;
+        DECLARE
+		@First VARCHAR(50),
+		@Last VARCHAR(50),
+		@Initials VARCHAR(50),
+		@ReturnStatus INT,
+		@ReturnMessage VARCHAR(1000),
+		@DTData_Id INT,
+		@Existing_DTReason_Id INT,
+		@Existing_DTMinutes FLOAT,
+		@Site_Timezone VARCHAR(100),
+		@Timestamp_UTC DATETIME,
+		@asset_id INT,
+		@Site_Id INT;
+        
+		SET @asset_id =
+        (
+            SELECT asset_id
+            FROM dbo.DxHData
+            WHERE dxhdata_id = @DxHData_Id
+        );
+        SELECT @Site_Id = asset_id
+        FROM [dbo].[Asset]
+        WHERE asset_level = 'Site'
+              AND site_code =
+        (
+            SELECT site_code
+            FROM [dbo].[Asset]
+            WHERE asset_id = @asset_id
+        );
+        SELECT @Site_Timezone = site_timezone
+        FROM dbo.CommonParameters cpt WITH(NOLOCK)
+        WHERE site_id = @Site_Id
+              AND STATUS = 'Active';
+
+		Select @Timestamp_UTC = @Timestamp at time zone @Site_Timezone at time zone 'UTC';
+
+        IF NOT EXISTS
+        (
+            SELECT dxhdata_id
+            FROM dbo.DxHData WITH(NOLOCK)
+            WHERE dxhdata_id = ISNULL(@DxHData_Id, -1)
+        )
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Invalid DxHData_Id ' + CONVERT(VARCHAR, ISNULL(@DxHData_Id, ''));
+                GOTO ErrExit;
+        END;
+        IF NOT EXISTS
+        (
+            SELECT dr.dtreason_id
+            FROM dbo.DTReason dr WITH(NOLOCK), 
+                 dbo.DxHData dxh WITH(NOLOCK)
+            WHERE dxh.dxhdata_id = @DxHData_Id
+                  AND dxh.asset_id = dr.asset_id
+                  AND dr.dtreason_id = ISNULL(@DTReason_Id, -1)
+        )
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Invalid DTReason_Id ' + CONVERT(VARCHAR, ISNULL(@DTReason_Id, ''));
+                GOTO ErrExit;
+        END;
+        IF(ISNULL(@DTMinutes, -1) < 0
+           OR ISNULL(@DTMinutes, -1) > 60.0)
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Invalid DTMinutes ' + CONVERT(VARCHAR, ISNULL(@DTMinutes, ''));
+                GOTO ErrExit;
+        END;
+        IF EXISTS
+        (
+            SELECT Badge
+            FROM dbo.TFDUsers WITH(NOLOCK)
+            WHERE Badge = ISNULL(@Clock_Number, -1)
+        )
+            BEGIN
+                SELECT @First = First_Name, 
+                       @Last = Last_Name
+                FROM dbo.TFDUsers WITH(NOLOCK)
+                WHERE Badge = @Clock_Number;
+                IF ISNULL(@First_Name, '') = ''
+                    BEGIN
+                        SET @First_Name = @First;
+                END;
+                IF ISNULL(@Last_Name, '') = ''
+                    BEGIN
+                        SET @Last_Name = @Last;
+                END;
+        END;
+        IF ISNULL(@First_Name, '') = ''
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Invalid First Name ' + CONVERT(VARCHAR, ISNULL(@First_Name, ''));
+                GOTO ErrExit;
+        END;
+        IF ISNULL(@Last_Name, '') = ''
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Invalid Last Name ' + CONVERT(VARCHAR, ISNULL(@Last_Name, ''));
+                GOTO ErrExit;
+        END;
+        SELECT @Initials = CONVERT(VARCHAR, LEFT(@First_Name, 1)) + CONVERT(VARCHAR, LEFT(@last_Name, 1));
+        IF ISDATE(@Timestamp) <> 1
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Invalid timestamp ' + CONVERT(VARCHAR, ISNULL(@Timestamp, ''));
+                GOTO ErrExit;
+        END;
+        IF(ISNULL(@Update, 0) <> 0)
+          AND (NOT EXISTS
+        (
+            SELECT dtdata_id
+            FROM dbo.DTData WITH(NOLOCK)
+            WHERE dtdata_id = ISNULL(@Update, -1)
+        ))
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Invalid Update ' + CONVERT(VARCHAR, ISNULL(@Update, ''));
+                GOTO ErrExit;
+        END;
+        IF ISNULL(@Update, 0) = 0
+            BEGIN
+                INSERT INTO dbo.DTData
+                (dxhdata_id, 
+                 dtreason_id, 
+                 dtminutes, 
+                 entered_by, 
+                 entered_on, 
+                 last_modified_by, 
+                 last_modified_on, 
+                 name
+                )
+                       SELECT @DxHData_Id, 
+                              @DTReason_Id, 
+                              @DTMinutes, 
+                              @Initials, 
+                              @Timestamp_UTC, 
+                              @Initials, 
+                              GETDATE(), 
+                              @First_Name + ' ' + @Last_Name;
+                SET @DTData_Id = SCOPE_IDENTITY();
+                SELECT @ReturnStatus = 0, 
+                       @ReturnMessage = 'Inserted ' + CONVERT(VARCHAR, @DTData_Id);
+        END;
+            ELSE
+            BEGIN
+                IF EXISTS
+                (
+                    SELECT dtdata_id
+                    FROM dbo.DTData WITH(NOLOCK)
+                    WHERE dtdata_id = ISNULL(@Update, -1)
+                )
+                    BEGIN
+                        SELECT @Existing_DTReason_Id = dtreason_id, 
+                               @Existing_DTMinutes = dtminutes
+                        FROM dbo.DTData WITH(NOLOCK)
+                        WHERE dtdata_id = ISNULL(@Update, -1);
+                        IF(ISNULL(@Existing_DTReason_Id, -1) <> ISNULL(@DTReason_Id, -1)
+                           OR ISNULL(@Existing_DTMinutes, -1) <> ISNULL(@DTMinutes, -1))
+                            BEGIN
+                                UPDATE dbo.DTData
+                                  SET 
+                                      dtreason_id = @DTReason_Id, 
+                                      dtminutes = @DTMinutes, 
+                                      last_modified_by = @Initials, 
+                                      last_modified_on = GETDATE(), 
+                                      name = @First_Name + ' ' + @Last_Name
+                                WHERE dtdata_id = @Update;
+                                SELECT @ReturnStatus = 0, 
+                                       @ReturnMessage = 'Updated ' + CONVERT(VARCHAR, ISNULL(@Update, ''));
+                        END;
+                            ELSE
+                            BEGIN
+                                SELECT @ReturnStatus = 0, 
+                                       @ReturnMessage = 'Nothing to update ' + CONVERT(VARCHAR, ISNULL(@Update, ''));
+                        END;
+                END;
+        END;
+        ErrExit:
+        IF @ReturnStatus IS NULL
+            BEGIN
+                SELECT @ReturnStatus = -1, 
+                       @ReturnMessage = 'Unknown Error';
+        END;
+        SELECT @ReturnStatus AS ReturnStatus, 
+               @ReturnMessage AS ReturnMessage;
+        RETURN;
+    END;
+
+        /****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Put_InterShiftData]    Script Date: 4/12/2019 15:25:17 ******/
+
+        SET ANSI_NULLS ON;
