8f14f54bea1d1ef09d5be94e2087415ae5b1fcf1
Andres Garcia <andy12garsa@gmail.com>
2021-01-28 12:45:27 -0600

    converted possible_end_time to varchar in scan sp

--- database/DeployDatabase/Parker_CTP_Dev/dbo/Stored Procedures/spLocal_EY_DxH_Get_Scan_By_Asset.sql
+++ database/DeployDatabase/Parker_CTP_Dev/dbo/Stored Procedures/spLocal_EY_DxH_Get_Scan_By_Asset.sql
@@ -1,4 +1,8 @@
 ﻿/****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Get_Scan_By_Asset]    Script Date: 27/1/2021 11:20:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
 /****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Get_Scan_By_Asset]    Script Date: 26/01/2021 11:31:05 ******/
 --
 -- Copyright © 2019 Ernst & Young LLP
@@ -36,9 +40,9 @@
 -- Modification Change History:
 --------------------------------------------------------------------------------	
 -- Example Call:
--- exec spLocal_EY_DxH_Get_Scan_By_Asset '2021-01-26 12:51:00','2021-01-27 12:51:00', 230
+-- exec spLocal_EY_DxH_Get_Scan_By_Asset '2021-01-27 10:00:00','2021-01-27 11:00:00', 230
 --
-CREATE   PROCEDURE [dbo].[spLocal_EY_DxH_Get_Scan_By_Asset]
+ALTER   PROCEDURE [dbo].[spLocal_EY_DxH_Get_Scan_By_Asset]
 @start_time       DATETIME, 
 @end_time		  DATETIME, 
 @asset_id         INT				
@@ -53,7 +57,7 @@ AS
                asset_id, 
                CONVERT(VARCHAR, start_time, 20) as start_time, 
                CONVERT(VARCHAR, end_time, 20) as end_time, 
-               possible_end_time,
+               CONVERT(VARCHAR, possible_end_time, 20) as possible_end_time,
 			   DATEDIFF(minute, possible_end_time, end_time) as minutes,
                is_current_scan, 
                reason, 
