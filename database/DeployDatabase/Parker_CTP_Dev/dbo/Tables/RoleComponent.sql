c0d829a16cefefcefbf20da7c19796569137c630
Andres Garcia <andy12garsa@gmail.com>
2021-02-02 12:26:19 -0600

    small changes in the rolecomponent table

--- database/DeployDatabase/Parker_CTP_Dev/dbo/Tables/RoleComponent.sql
+++ database/DeployDatabase/Parker_CTP_Dev/dbo/Tables/RoleComponent.sql
@@ -7,5 +7,6 @@
 	[can_write] BIT NOT NULL DEFAULT 0,
 	CONSTRAINT [PK_RoleComponent_Id] PRIMARY KEY CLUSTERED ([rolecomponent_id] ASC),
 	CONSTRAINT [FK_Role_Role_Id] FOREIGN KEY ([role_id]) REFERENCES [dbo].[Role] ([role_id]),
-	CONSTRAINT [FK_Role_Component_Id] FOREIGN KEY ([component_id]) REFERENCES [dbo].[Component] ([component_id])
+	CONSTRAINT [FK_Role_Component_Id] FOREIGN KEY ([component_id]) REFERENCES [dbo].[Component] ([component_id]),
+	CONSTRAINT [UQRoleComponent] UNIQUE ([role_id],[component_id])
 )
