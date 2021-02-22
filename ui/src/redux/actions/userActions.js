8b6b17a3ab37a7d5a1b3c187f613e2f31e779491
Jose Carlos Rodriguez Arrieta <Jose.C.Rodriguez.Arrieta@cr.ey.com>
2021-02-22 09:25:31 -0600

    Setting redux

---
+++ ui/src/redux/actions/userActions.js
@@ -0,0 +1,15 @@
+import Axios from "axios";
+import { GET_DASHBOARD_INFO } from "../constants/constants";
+import { API } from "../../Utils/Constants";
+
+export const getDashboardInfo = (siteId) => {
+  var url = `${API}/total_rows?${siteId}`;
+  return (dispatch) => {
+    return Axios.get(url).then((response) => {
+      dispatch({
+        type: GET_DASHBOARD_INFO,
+        dashboardData: response,
+      });
+    });
+  };
+};
---
+++ ui/src/redux/reducer/user.js
@@ -0,0 +1,20 @@
+import { GET_DASHBOARD_INFO } from "../constants/constants";
+
+const initialState = {
+  dashboardData: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_DASHBOARD_INFO:
+      return {
+        ...state,
+        dashboardData: [...action.dashboardData],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const GetDashboardInfo = state => state.userReducer.dashboardData;
