5f9d9c1bf17e84eb52f4506014cf56e55bf94d26
Andres Garcia <andy12garsa@gmail.com>
2021-03-03 16:52:21 -0600

    ordered the users by last name and first name

--- api/src/repositories/user-repository.ts
+++ api/src/repositories/user-repository.ts
@@ -20,7 +20,8 @@ export class UserRepository {
         JOIN dbo.Asset A 
         ON TFD.Site = A.asset_id AND TFD.Site = ${site_id}
         LEFT JOIN dbo.Escalation E
-        ON TFD.escalation_id = E.escalation_id`);
+        ON TFD.escalation_id = E.escalation_id
+        ORDER BY TFD.Last_Name, TFD.First_Name`);
     }
     public async findUserBySiteAndRole(site_id: number, role_id: number): Promise<any> {
         return await this.sqlServerStore.ExecuteQuery(`SELECT TFD.Badge, TFD.Username, TFD.First_Name, TFD.Last_Name, R.name, TFD.role_id, TFD.status,
@@ -30,7 +31,8 @@ export class UserRepository {
         AND TFD.Site = ${site_id}
         AND TFD.role_id = ${role_id}
         LEFT JOIN dbo.Escalation E
-        ON TFD.escalation_id = E.escalation_id`);
+        ON TFD.escalation_id = E.escalation_id
+        ORDER BY TFD.Last_Name, TFD.First_Name`);
     }
     public async findUserBySiteAndStatus(site_id: number, status: string): Promise<any> {
         return await this.sqlServerStore.ExecuteQuery(`SELECT TFD.Badge, TFD.Username, TFD.First_Name, TFD.Last_Name, R.name, TFD.role_id, TFD.status,
@@ -40,7 +42,8 @@ export class UserRepository {
         AND TFD.Site = ${site_id}
         AND TFD.status = '${status}'
         LEFT JOIN dbo.Escalation E
-        ON TFD.escalation_id = E.escalation_id`);
+        ON TFD.escalation_id = E.escalation_id
+        ORDER BY TFD.Last_Name, TFD.First_Name`);
     }
     public async findUserBySiteAndStatuAndRole(site_id: number, status: string, role_id: number): Promise<any> {
         return await this.sqlServerStore.ExecuteQuery(`SELECT TFD.Badge, TFD.Username, TFD.First_Name, TFD.Last_Name, R.name, TFD.role_id, TFD.status,
@@ -51,7 +54,8 @@ export class UserRepository {
         AND TFD.status = '${status}'
         AND TFD.role_id = ${role_id},
         LEFT JOIN dbo.Escalation E
-        ON TFD.escalation_id = E.escalation_id`);
+        ON TFD.escalation_id = E.escalation_id
+        ORDER BY TFD.Last_Name, TFD.First_Name`);
     }
     public async findUserByBadgeAndSite(site_id: number, badge: string): Promise<any> {
         return await this.sqlServerStore.ExecuteQuery(`SELECT TFD.Badge, TFD.Username, TFD.First_Name, TFD.Last_Name, R.name, TFD.role_id, TFD.status,
@@ -61,7 +65,8 @@ export class UserRepository {
         AND TFD.Site = ${site_id}
         AND TFD.Badge = '${badge}'
         LEFT JOIN dbo.Escalation E
-        ON TFD.escalation_id = E.escalation_id`);
+        ON TFD.escalation_id = E.escalation_id
+        ORDER BY TFD.Last_Name, TFD.First_Name`);
     }
     public async putUser(badge: string, username: string, first_name: string, last_name: string, role_id: number, status: string, site_id: number, escalation_id: number): Promise<any> {
         return await this.sqlServerStore.ExecuteQuery(`exec spLocal_EY_DxH_Put_Users N'${badge}', N'${username}', N'${first_name}', N'${last_name}', ${role_id}, N'${status}', ${site_id}, ${escalation_id}`);
