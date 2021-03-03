e18c0f4ac3ea1b41eeb17164c7b6132bc60addad
Jose Carlos Rodriguez Arrieta <Jose.C.Rodriguez.Arrieta@cr.ey.com>
2021-03-03 16:31:47 -0600

    Working on shifts module

--- ui/src/redux/actions/shiftsActions.js
+++ ui/src/redux/actions/shiftsActions.js
@@ -1,18 +1,16 @@
 import Axios from "axios";
-import {
-  GET_SHIFTS
-} from "../constants/constants";
+import { GET_SHIFTS } from "../constants/constants";
 import { API } from "../../Utils/Constants";
 
 export const getShifts = (siteId) => {
-    var url = `${API}/users?site_id=${siteId}`;
-    return (dispatch) => {
-      return Axios.get(url).then((response) => {
-        dispatch({
-          type: GET_SHIFTS,
-          shifts: response.data,
-        });
-        return response.data;
+  var url = `${API}/users?site_id=${siteId}`;
+  return (dispatch) => {
+    return Axios.get(url).then((response) => {
+      dispatch({
+        type: GET_SHIFTS,
+        shifts: response.data,
       });
-    };
+      return response.data;
+    });
   };
+};
