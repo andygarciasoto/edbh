4810ebb870afcf704f5cb8f0ae2cac072f8fe08d
Andres Garcia <andy12garsa@gmail.com>
2021-03-09 13:48:03 -0600

    order assembly receiving URL from db

--- api/src/services/orderdataservice.ts
+++ api/src/services/orderdataservice.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { OrderDataRepository } from '../repositories/orderdata-repository';
 import { AssetRepository } from '../repositories/asset-repository';
 import { ProductRepository } from '../repositories/product-repository';
+import { CommonParametersRepository } from '../repositories/commonparameters-repository';
 let request = require('request');
 
 export class OrderDataService {
@@ -9,11 +10,14 @@ export class OrderDataService {
     private readonly orderdatarepository: OrderDataRepository;
     private readonly assetrepository: AssetRepository;
     private readonly productrepository: ProductRepository;
+    private readonly commonparametersrepository: CommonParametersRepository;
 
-    public constructor(orderdatarepository: OrderDataRepository, assetrepository: AssetRepository, productrepository: ProductRepository) {
+    public constructor(orderdatarepository: OrderDataRepository, assetrepository: AssetRepository, productrepository: ProductRepository,
+        commonparametersrepository: CommonParametersRepository) {
         this.orderdatarepository = orderdatarepository;
         this.assetrepository = assetrepository;
         this.productrepository = productrepository;
+        this.commonparametersrepository = commonparametersrepository;
     }
 
     public async getOrderAssembly(req: Request, res: Response) {
@@ -21,37 +25,43 @@ export class OrderDataService {
         if (params.order_number === undefined || params.asset_code === undefined || params.timestamp === undefined) {
             return res.status(400).json({ message: "Bad Request - Missing Parameters" });
         }
-
+        let assembly: any;
         let asset: any;
         let orderData: any;
         try {
-            asset = await this.assetrepository.getAssetByCode(params.asset_code);
-            orderData = await this.orderdatarepository.getOrderData(asset[0].asset_id, params.order_number);
-            const orderId = orderData[0] ? orderData[0].order_id : orderData[0];
-            if (orderId === null || orderId === undefined) {
-                let assembly = {
-                    order_number: params.order_number,
-                    asset_code: params.asset_code,
-                    timestamp: params.timestamp,
-                    message_source: "assembly"
-                };
-                request({
-                    url: "http://tfd036w04.us.parker.corp/jTrax/DxHTrigger/api/assemblyorder",
-                    method: "POST",
-                    json: true,
-                    body: assembly,
-                    timeout: 10000
-                }, (error, resp, body) => {
-                    if (error || body) {
-                        return res.status(500).send({ message: 'Error', jtrax_error: error.Message });
-                    }
-                    if (resp.statusCode >= 400) {
-                        return res.status(500).send({ message: 'Error', jtrax_error: error.Message, body: body.Message });
-                    }
-                    return res.status(200).json({});
-                });
+            assembly = await this.commonparametersrepository.getAssemblyUrl(params.asset_code);
+            const url = assembly[0] !== undefined ? assembly[0].assembly_url : undefined;
+            if (url === undefined || url === null || !url) {
+                return res.status(400).json({ message: "Bad Request - Site doesn't have a correct assembly URL" });
             } else {
-                return res.status(200).json(orderData);
+                asset = await this.assetrepository.getAssetByCode(params.asset_code);
+                orderData = await this.orderdatarepository.getOrderData(asset[0].asset_id, params.order_number);
+                const orderId = orderData[0] ? orderData[0].order_id : orderData[0];
+                if (orderId === null || orderId === undefined) {
+                    let assembly = {
+                        order_number: params.order_number,
+                        asset_code: params.asset_code,
+                        timestamp: params.timestamp,
+                        message_source: "assembly"
+                    };
+                    request({
+                        url: url,
+                        method: "POST",
+                        json: true,
+                        body: assembly,
+                        timeout: 10000
+                    }, (error, resp, body) => {
+                        if (error || body) {
+                            return res.status(500).send({ message: 'Error', jtrax_error: error.Message });
+                        }
+                        if (resp.statusCode >= 400) {
+                            return res.status(500).send({ message: 'Error', jtrax_error: error.Message, body: body.Message });
+                        }
+                        return res.status(200).json({});
+                    });
+                } else {
+                    return res.status(200).json(orderData);
+                }
             }
         } catch (err) {
             return res.status(500).json({ message: err.message });
