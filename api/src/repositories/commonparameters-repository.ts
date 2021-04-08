538e84b0530236d9745fddec1fe511ebbfee9cf7
Brayan <brayan.gonzalez.marchena@cr.ey.com>
2021-04-08 10:46:04 -0600

    Update common parameters import export

--- api/src/repositories/commonparameters-repository.ts
+++ api/src/repositories/commonparameters-repository.ts
@@ -9,10 +9,9 @@ export class CommonParametersRepository {
     }
 
     public async getCommonParametersBySite(site_id: number): Promise<any> {
-        return await this.sqlServerStore.ExecuteQuery(`SELECT [Asset].[asset_code] AS site_code,[CommonParameters].[site_name],[CommonParameters].[production_day_offset_minutes],[CommonParameters].[site_timezone],
-        [CommonParameters].[ui_timezone],[CommonParameters].[escalation_level1_minutes],[CommonParameters].[escalation_level2_minutes],
-        [CommonParameters].[default_target_percent_of_ideal],[CommonParameters].[default_setup_minutes],[CommonParameters].[default_routed_cycle_time],
-        [CommonParameters].[setup_lookback_minutes],[CommonParameters].[language],[CommonParameters].[status],
+        return await this.sqlServerStore.ExecuteQuery(`SELECT [Asset].[asset_code] AS site_code,[CommonParameters].[site_name],[CommonParameters].[production_day_offset_minutes],
+        [CommonParameters].[site_timezone],[CommonParameters].[ui_timezone],[CommonParameters].[default_target_percent_of_ideal],[CommonParameters].[default_setup_minutes],
+        [CommonParameters].[default_routed_cycle_time],[CommonParameters].[language],[CommonParameters].[status],
         [CommonParameters].[entered_by],[CommonParameters].[entered_on],[CommonParameters].[last_modified_by],[CommonParameters].[last_modified_on],
         [CommonParameters].[summary_timeout],[CommonParameters].[break_minutes],[CommonParameters].[lunch_minutes],[CommonParameters].[site_prefix],
         [CommonParameters].[assembly_url]
