e6c202904a3dd5fde2a331245fd3222cf8843f24
Andres Garcia <andy12garsa@gmail.com>
2021-04-08 16:34:48 -0600

    all Administration tool endpoints added

--- database/DeployDatabase/Parker_CTP_Dev/dbo/Stored Procedures/spLocal_EY_DxH_Put_Asset.sql
+++ database/DeployDatabase/Parker_CTP_Dev/dbo/Stored Procedures/spLocal_EY_DxH_Put_Asset.sql
@@ -1,158 +1,96 @@
-﻿
+﻿/****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Put_Asset]    Script Date: 8/4/2021 09:14:38 ******/
 
+CREATE    PROCEDURE [dbo].[spLocal_EY_DxH_Put_Asset] (
+	@asset_id					as INT,
+	@asset_code					as NVARCHAR(100),			
+	@asset_name					as NVARCHAR(200),	
+	@asset_description			as NVARCHAR(256),	
+	@asset_level				as NVARCHAR(100),	
+	@site_code					as NVARCHAR(100),	
+	@parent_asset_code			as NVARCHAR(100),
+	@automation_level			as NVARCHAR(100),
+	@include_in_escalation		as BIT,
+	@grouping1					as NVARCHAR(256),
+	@grouping2					as NVARCHAR(256),
+	@grouping3    				as NVARCHAR(256),
+	@grouping4					as NVARCHAR(256),
+	@grouping5					as NVARCHAR(256),
+	@status						as NVARCHAR(50),
+	@target_percent_of_ideal	as FLOAT,
+	@is_multiple				as BIT)
+	
+AS  BEGIN 
 
-CREATE    PROCEDURE [dbo].[spLocal_EY_DxH_Put_Asset] (@asset_code as NVARCHAR(100),			
-	@asset_name				as NVARCHAR(200),	
-	@asset_description		as NVARCHAR(256),	
-	@asset_level			as NVARCHAR(100),	
-	@site_code				as NVARCHAR(100),	
-	@parent_asset_code		as NVARCHAR(100),		
-	@value_stream			as NVARCHAR(100),
-	@automation_level		as NVARCHAR(100),
-	@include				as NVARCHAR(100),
-	@grouping1				as NVARCHAR(256),
-	@grouping2				as NVARCHAR(256),
-	@grouping3    			as NVARCHAR(256),
-	@grouping4				as NVARCHAR(256),
-	@grouping5				as NVARCHAR(256),
-	@status					as NVARCHAR(50),
-	@entered_by				as NVARCHAR(100),
-	@entered_on				as Datetime,
-	@last_modified_by		as NVARCHAR(100),
-	@last_modified_on		as Datetime)
-
- AS  BEGIN 
- DECLARE
- @include_in_escalation as bit
-
- IF @asset_description = 'NULL'
- BEGIN
- SET @asset_description = NULL
- END
-
- IF @value_stream = 'NULL'
- BEGIN
- SET @value_stream = NULL
- END
-
- IF @automation_level = 'NULL'
- BEGIN
- SET @automation_level = NULL
- END
-
- IF @include = 'NULL'
- BEGIN
- SET @include = NULL
- END
-
- IF @include = '0'
- BEGIN
- SET @include_in_escalation = 0
- END
-
- IF @include = '1'
- BEGIN
- SET @include_in_escalation = 1
- END
- 
- IF @grouping1 = 'NULL'
- BEGIN
- SET @grouping1 = NULL
- END
-
- IF @grouping2 = 'NULL'
- BEGIN
- SET @grouping2 = NULL
- END
-
- IF @grouping3 = 'NULL'
- BEGIN
- SET @grouping3 = NULL
- END
-
- IF @grouping4 = 'NULL'
- BEGIN
- SET @grouping4 = NULL
- END
-
- IF @grouping5 = 'NULL'
- BEGIN
- SET @grouping5 = NULL
- END
-
-IF EXISTS (SELECT asset_code FROM dbo.Asset
-WHERE
-asset_code = @asset_code)
+IF EXISTS (SELECT asset_id FROM dbo.Asset
+	WHERE
+	asset_id = @asset_id
+	AND site_code = @site_code)
 BEGIN
-UPDATE [dbo].[Asset]
-   SET [asset_name] = @asset_name
-      ,[asset_description] = @asset_description
-      ,[asset_level] = @asset_level
-      ,[site_code] = @site_code
-      ,[parent_asset_code] = @parent_asset_code
-      ,[value_stream] = @value_stream
-      ,[automation_level] = @automation_level
-      ,[include_in_escalation] = @include_in_escalation
-	  ,[grouping1] = @grouping1
-      ,[grouping2] = @grouping2
-      ,[grouping3] = @grouping3
-      ,[grouping4] = @grouping4
-      ,[grouping5] = @grouping5
-      ,[status] = @status
-      ,[entered_by] = 'SQL manual entry'
-      ,[entered_on] = @entered_on
-      ,[last_modified_by] = 'SQL manual entry'
-      ,[last_modified_on] = getDate()
- WHERE asset_code = @asset_code
+	UPDATE [dbo].[Asset]
+	   SET [asset_name] = @asset_name
+		  ,[asset_description] = @asset_description
+		  ,[asset_level] = @asset_level
+		  ,[parent_asset_code] = @parent_asset_code
+		  ,[automation_level] = @automation_level
+		  ,[include_in_escalation] = @include_in_escalation
+		  ,[grouping1] = @grouping1
+		  ,[grouping2] = @grouping2
+		  ,[grouping3] = @grouping3
+		  ,[grouping4] = @grouping4
+		  ,[grouping5] = @grouping5
+		  ,[status] = @status
+		  ,[entered_by] = 'Administration Tool'
+		  ,[entered_on] = GETDATE()
+		  ,[last_modified_by] = 'Administration Tool'
+		  ,[last_modified_on] = getDate()
+		  ,[target_percent_of_ideal] = @target_percent_of_ideal
+		  ,[is_multiple] = @is_multiple
+	WHERE asset_id = @asset_id AND site_code = @site_code
+	END
+ELSE
+	BEGIN
+	INSERT INTO [dbo].[Asset]
+			   ([asset_code]
+			   ,[asset_name]
+			   ,[asset_description]
+			   ,[asset_level]
+			   ,[site_code]
+			   ,[parent_asset_code]
+			   ,[automation_level]
+			   ,[include_in_escalation]
+			   ,[grouping1]
+			   ,[grouping2]
+			   ,[grouping3]
+			   ,[grouping4]
+			   ,[grouping5]
+			   ,[status]
+			   ,[entered_by]
+			   ,[entered_on]
+			   ,[last_modified_by]
+			   ,[last_modified_on]
+			   ,[target_percent_of_ideal]
+			   ,[is_multiple])
+		 VALUES
+			   (@asset_code
+			   ,@asset_name
+			   ,@asset_description
+			   ,@asset_level
+			   ,@site_code
+			   ,@parent_asset_code
+			   ,@automation_level
+			   ,@include_in_escalation
+			   ,@grouping1
+			   ,@grouping2
+			   ,@grouping3
+			   ,@grouping4
+			   ,@grouping5
+			   ,@status
+			   ,'Administration Tool'
+			   , GETDATE()
+			   , 'Administration Tool'
+			   , GETDATE()
+			   ,@target_percent_of_ideal
+			   ,@is_multiple)
+	END
 END
 
-IF NOT EXISTS (SELECT asset_code FROM dbo.Asset
-WHERE
-asset_code = @asset_code)
-BEGIN
-INSERT INTO [dbo].[Asset]
-           ([asset_code]
-           ,[asset_name]
-           ,[asset_description]
-           ,[asset_level]
-           ,[site_code]
-           ,[parent_asset_code]
-           ,[value_stream]
-           ,[automation_level]
-           ,[include_in_escalation]
-           ,[grouping1]
-           ,[grouping2]
-           ,[grouping3]
-           ,[grouping4]
-           ,[grouping5]
-           ,[status]
-           ,[entered_by]
-           ,[entered_on]
-           ,[last_modified_by]
-           ,[last_modified_on])
-     VALUES
-           (@asset_code
-           ,@asset_name
-           ,@asset_description
-           ,@asset_level
-           ,@site_code
-           ,@parent_asset_code
-           ,@value_stream
-           ,@automation_level
-           ,@include_in_escalation
-           ,@grouping1
-           ,@grouping2
-           ,@grouping3
-           ,@grouping4
-           ,@grouping5
-           ,@status
-           ,'SQL manual entry'
-           , getDate()
-           , 'SQL manual entry'
-           , getDate())
-END
-END
-
-
-/****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Put_AssetDisplaySystem]    Script Date: 4/12/2019 15:21:36 ******/
-SET ANSI_NULLS ON
