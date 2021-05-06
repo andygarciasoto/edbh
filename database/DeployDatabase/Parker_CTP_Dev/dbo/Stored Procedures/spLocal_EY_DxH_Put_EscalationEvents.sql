387b3b97043aef3b0ed25d5356d0771306c061d3
Andres Garcia <andy12garsa@gmail.com>
2021-05-06 16:08:15 -0600

    added new escalation logic to the db project (ctp)

---
+++ database/DeployDatabase/Parker_CTP_Dev/dbo/Stored Procedures/spLocal_EY_DxH_Put_EscalationEvents.sql
@@ -0,0 +1,52 @@
+﻿/****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Put_EscalationEvents]    Script Date: 6/5/2021 15:42:54 ******/
+
+/****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Put_EscalationEvents]    Script Date: 20/4/2021 14:58:33 ******/
+
+/****** Object:  StoredProcedure [dbo].[spLocal_EY_DxH_Put_EscalationEvents]    Script Date: 31/12/2020 09:35:04 ******/
+
+-- Example Call:
+-- exec spLocal_EY_DxH_Put_EscalationEvents 'Site Manager', 'Group 2', 1, 2
+
+CREATE PROCEDURE [dbo].[spLocal_EY_DxH_Put_EscalationEvents] 
+	@dxhdata_id			AS INT,
+	@asset_id			AS INT,
+	@escalation_time	AS DATETIME,			
+	@sign_time			AS DATETIME,	
+	@badge				AS NVARCHAR(100),
+	@site_id			AS INT,
+	@escalation_id		AS INT
+
+AS  BEGIN 
+
+	IF EXISTS (SELECT dxhdata_id FROM dbo.EscalationEvents
+	WHERE
+	dxhdata_id = @dxhdata_id AND @escalation_time IS NULL)
+		BEGIN
+			UPDATE dbo.EscalationEvents
+			SET 
+			sign_time = @sign_time,
+			badge = @badge
+			WHERE
+			dxhdata_id = @dxhdata_id AND @escalation_time IS NULL
+		END
+	ELSE
+		BEGIN
+			IF NOT EXISTS (SELECT dxhdata_id FROM dbo.EscalationEvents
+			WHERE
+			dxhdata_id = @dxhdata_id)
+			BEGIN
+				INSERT INTO dbo.EscalationEvents
+			   (dxhdata_id
+			   ,asset_id
+			   ,escalation_time
+			   ,site
+			   ,escalation_id)
+			VALUES
+			   (@dxhdata_id
+			   ,@asset_id
+			   ,@escalation_time
+			   ,@site_id
+			   ,@escalation_id)
+			END
+		END
+	END
