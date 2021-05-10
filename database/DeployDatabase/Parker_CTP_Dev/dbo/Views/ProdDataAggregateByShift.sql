8e1e7ca410587f12995ff06a40dfc34b90d05924
Brayan <brayan.gonzalez.marchena@cr.ey.com>
2021-05-10 12:30:47 -0600

    Update database project and config deploy file

--- database/DeployDatabase/Parker_CTP_Dev/dbo/Views/ProdDataAggregateByShift.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE VIEW [dbo].[ProdDataAggregateByShift]
-AS
-SELECT        dbo.DxHData.production_day, SUM(dbo.ProductionData.target) AS TargetPieces, SUM(dbo.ProductionData.actual) AS ActualPieces, dbo.DxHData.shift_code, CASE WHEN SUM(dbo.ProductionData.actual) 
-                         < SUM(dbo.ProductionData.target) THEN 'Behind' ELSE 'Ahead' END AS Status, dbo.Asset.asset_code
-FROM            dbo.DxHData INNER JOIN
-                         dbo.ProductionData ON dbo.DxHData.dxhdata_id = dbo.ProductionData.dxhdata_id INNER JOIN
-                         dbo.Asset ON dbo.DxHData.asset_id = dbo.Asset.asset_id
-GROUP BY dbo.DxHData.asset_id, dbo.DxHData.production_day, dbo.DxHData.shift_code, dbo.DxHData.hour_interval, dbo.DxHData.asset_id, dbo.Asset.asset_code
