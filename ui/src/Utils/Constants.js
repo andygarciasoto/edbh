76b551cfe66ec87c78289d482ba2b910c0c28b91
Andres Garcia <andres.garcia.soto1@ey.com>
2022-01-11 17:15:03 +0000

    new urls for azure stack

--- ui/src/Utils/Constants.js
+++ ui/src/Utils/Constants.js
@@ -1,7 +1,7 @@
-//  export const API = 'http://localhost:8080/api';
-//  export const AUTH = 'http://localhost:8080/auth';
-//  export const DATATOOL = 'http://localhost:8080/datatool';
-//  export const SOCKET = 'http://localhost:8080';
+export const API = 'http://localhost:8080/api';
+export const AUTH = 'http://localhost:8080/auth';
+export const DATATOOL = 'http://localhost:8080/datatool';
+export const SOCKET = 'http://localhost:8080';
 
 // sf : shift
 // dt : date 
@@ -10,26 +10,8 @@
 // tp : machine_type | automation_level
 
 
-//public app
-// export const API = 'https://dxhapitest.azurewebsites.net/api';
-// export const AUTH = 'https://dxhapitest.azurewebsites.net/auth';
-// export const DATATOOL = 'https://dxhapitest.azurewebsites.net/datatool';
-// export const SOCKET = 'https://dxhapitest.azurewebsites.net';
-
-//private Test
-// export const API = 'https://edxhapitst.us.parker.corp/api';
-// export const AUTH = 'https://edxhapitst.us.parker.corp/auth';
-// export const DATATOOL = 'https://edxhapitst.us.parker.corp/datatool';
-// export const SOCKET = 'https://edxhapitst.us.parker.corp';
-
-//private Prod
-// export const API = 'https://parkerdaybyhourapi.us.parker.corp/api';
-// export const AUTH = 'https://parkerdaybyhourapi.us.parker.corp/auth';
-// export const DATATOOL = 'https://parkerdaybyhourapi.us.parker.corp/datatool';
-// export const SOCKET = 'https://parkerdaybyhourapi.us.parker.corp';
-
-//NSIH APP
-export const API = 'https://euwdsrg02krsg01app02.azurewebsites.net/api';
-export const AUTH = 'https://euwdsrg02krsg01app02.azurewebsites.net/auth';
-export const DATATOOL = 'https://euwdsrg02krsg01app02.azurewebsites.net/datatool';
-export const SOCKET = 'https://euwdsrg02krsg01app02.azurewebsites.net';
+//Azure Stack
+// export const API = 'https://edbh-api.kube-environment-vforyk2.eastus.k4apps.io/api';
+// export const AUTH = 'https://edbh-api.kube-environment-vforyk2.eastus.k4apps.io/auth';
+// export const DATATOOL = 'https://edbh-api.kube-environment-vforyk2.eastus.k4apps.io/datatool';
+// export const SOCKET = 'https://edbh-api.kube-environment-vforyk2.eastus.k4apps.io';
